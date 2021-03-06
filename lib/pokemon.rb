aceefb3cc2e4832bd35ff84ce9b24f8e36f60d79
Dexter <dex429.dc@gmail.com>
2021-03-05 20:40:34 -0500

    commit tree 1e21af737f42b689f7bb41e6c1b918e2b354431f

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,49 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+         INSERT INTO pokemon(name, type)
+         VALUES (?, ?)
+        SQL
+        db.execute(sql, [name, type])
+    end
+
+    def new_from_db
+        sql = <<-SQL
+          SELECT *
+          FROM pokemon
+        SQL
+    
+        DB[:conn].execute(sql).map do |row|
+          self.new_from_db(row)
+        end
+      end
+
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        LIMIT 1
+        SQL
+    
+        pokemon = db.execute(sql, id).map do |row|
+            self.new_from_db(row)
+          end.first
+
+          
+    end
+
+    
+    
 end
